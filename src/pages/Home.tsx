df434882f68be812d77ed221c232012f776ff0e9
Tiago Andrade <tiagoandradedejoao@gmail.com>
2024-05-16 20:35:12 -0300

    :sparkles: feat(home): add new movies section

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,33 @@
-import Carousel from "../components/Carousel";
+import { useEffect, useState } from "react";
 import Hero from "../components/Hero";
-import MovieCard from "../components/movie/MovieCard";
+import FeaturedMovie from "../components/movie/FeaturedMovie";
+import { movieAPI } from  "../services/movie-api"
+import { Movie, MovieList } from "../interfaces/movies-api/movie-list";
+import MovieListSection from "../components/MovieListSection";
 
 export function Home() {
+  const [releases, setReleases] = useState<Movie[]>([])
+
+  useEffect(() => {
+    movieAPI.get('movie/now_playing', {
+      params: {
+        language: 'pt',
+        page: 1,
+      },
+    })
+      .then(json => json.data)
+      .then((data: MovieList) => {
+        setReleases(data.results)
+      })
+  }, [])
+
   return (
-    <>
+    <div className="flex flex-col gap-2">
       <Hero />
 
-      <Carousel>
-        {new Array(15).fill(<MovieCard />)}
-      </Carousel>
-    </>
+      <MovieListSection title="Lançamentos" movieList={releases} />
+
+      <FeaturedMovie />
+    </div>
   )
 }
