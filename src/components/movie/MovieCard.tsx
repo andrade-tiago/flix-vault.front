bccf7c2dd4ef37bac3c7553cbfaa4ee1139ba7c1
Tiago Andrade <tiagoandradedejoao@gmail.com>
2024-05-16 20:18:00 -0300

    :recycle: refactor: add movie api support to components

--- src/components/movie/MovieCard.tsx
+++ src/components/movie/MovieCard.tsx
@@ -1,29 +1,38 @@
 import MovieData from "./MovieData"
 import PlayButton from "./PlayButton"
+import MovieDataProps from "../../interfaces/movie-data-props"
 
-export default function MovieCard() {
-  const movieImg = "https://i.pinimg.com/736x/3c/fc/99/3cfc99925ade431e3b9c3ae71ccc1c3d.jpg"
-  const movieTitle = "Rougue One: Uma história Star Wars"
-  const movieDescription = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In pellentesque commodo diam, in facilisis mi eleifend eget. Suspendisse eu ante eu metus tincidunt vulputate. Ut libero lacus, feugiat ac velit eu, porttitor elementum quam."
+interface MovieCardProps extends MovieDataProps {
+  title: string
+  description: string
+  imgPath: string
+}
+
+export default function MovieCard(props: MovieCardProps) {
+  const imgURL = 'https://image.tmdb.org/t/p/w300' + props.imgPath // '/img.jpg'
+  const titleLengthLimit = 15
 
   return (
     <div className="w-44 hover:-translate-y-1 focus-within:-translate-y-1 transition-transform duration-200 flex flex-col gap-1 text-gray-400">
       <div className="h-64 rounded-xl overflow-hidden relative">
         {/* background-image */} <img
-          src={movieImg} alt="Banner do filme"
+          src={imgURL} alt="Banner do filme"
           className="absolute w-full h-full object-cover -z-10 top-0 left-0"
         />
 
         <div className="w-full h-full p-3 bg-gray-950/70 backdrop-blur-sm opacity-0 hover:opacity-100 transition-opacity focus-within:opacity-100 flex flex-col justify-between items-center">
-          <p className="w-full text-gray-100">{movieDescription.substring(0, 100)}...</p>
+          <p className="w-full text-gray-100">{props.description.substring(0, 100)}...</p>
 
           <PlayButton />
         </div>
       </div>
 
-      <h3 className="font-medium w-full" title={movieTitle}>{movieTitle.substring(0, 15)}...</h3>
+      <h3 className="font-medium w-full" title={props.title}>
+        {props.title.substring(0, titleLengthLimit)}
+        {props.title.length > titleLengthLimit && '...'}
+      </h3>
 
-      <MovieData year={2015} duration={78} rating={8} />
+      <MovieData year={props.year} duration={props.duration} rating={props.rating} />
     </div>
   )
 }
--- src/components/movie/MovieData.tsx
+++ src/components/movie/MovieData.tsx
@@ -1,22 +1,17 @@
 import { Star } from "lucide-react";
+import MovieDataProps from "../../interfaces/movie-data-props";
 
-interface MovieDataProps {
-  year: number
-  duration: number
-  rating: number
-}
-
-export default function MovieData({year, duration, rating}: MovieDataProps) {
+export default function MovieData(props: MovieDataProps) {
   return (
     <div className="flex items-baseline w-full max-w-48 justify-between">
-      <span title="Ano de lançamento">{year}</span>
+      <span title="Ano de lançamento">{props.year}</span>
 
-      <span title="Duração">{duration} min</span>
+      <span title="Duração">{props.duration} min</span>
 
       <div title="Avaliação" className="flex items-baseline gap-1">
         <Star className="text-yellow-500 size-5 translate-y-0.5" />
         <div>
-          <span className="text-lg">{rating.toFixed(1)}</span>
+          <span className="text-lg">{props.rating.toFixed(1)}</span>
           <span>/10</span>
         </div>
       </div>
