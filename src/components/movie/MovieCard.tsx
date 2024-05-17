ca78aebfd213a3d52181fcace485c5c1bc9185da
Tiago Andrade <tiagoandradedejoao@gmail.com>
2024-05-16 23:26:31 -0300

    :sparkles: feat: add truncate title and description too long

--- src/components/movie/MovieCard.tsx
+++ src/components/movie/MovieCard.tsx
@@ -1,6 +1,7 @@
 import MovieData from "./MovieData"
 import PlayButton from "./PlayButton"
 import MovieDataProps from "../../interfaces/movie-data-props"
+import truncateText from "../../utils/truncate-text"
 
 interface MovieCardProps extends MovieDataProps {
   title: string
@@ -10,7 +11,6 @@ interface MovieCardProps extends MovieDataProps {
 
 export default function MovieCard(props: MovieCardProps) {
   const imgURL = 'https://image.tmdb.org/t/p/w300' + props.imgPath // '/img.jpg'
-  const titleLengthLimit = 15
 
   return (
     <div className="w-44 hover:-translate-y-1 focus-within:-translate-y-1 transition-transform duration-200 flex flex-col gap-1 text-gray-400">
@@ -21,15 +21,16 @@ export default function MovieCard(props: MovieCardProps) {
         />
 
         <div className="w-full h-full p-3 bg-gray-950/70 backdrop-blur-sm opacity-0 hover:opacity-100 transition-opacity focus-within:opacity-100 flex flex-col justify-between items-center">
-          <p className="w-full text-gray-100">{props.description.substring(0, 100)}...</p>
+          <p className="w-full text-gray-100">
+            {truncateText(props.description, 100)}
+          </p>
 
           <PlayButton />
         </div>
       </div>
 
       <h3 className="font-medium w-full" title={props.title}>
-        {props.title.substring(0, titleLengthLimit)}
-        {props.title.length > titleLengthLimit && '...'}
+        {truncateText(props.title, 20)}
       </h3>
 
       <MovieData year={props.year} duration={props.duration} rating={props.rating} />
