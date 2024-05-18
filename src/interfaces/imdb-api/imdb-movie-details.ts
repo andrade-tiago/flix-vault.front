76ead811f8dc34398b3806f7b86b38904a73eead
Tiago Andrade <tiagoandradedejoao@gmail.com>
2024-05-18 18:16:12 -0300

    :label: feat: add more movie api callback interfaces

---
+++ src/interfaces/imdb-api/imdb-movie-details.ts
@@ -0,0 +1,52 @@
+import IMDbGenre from "./imdb-genre"
+
+interface ProductionCompany {
+  id: number
+  logo_path: string
+  name: string
+  origin_country: string
+}
+
+interface ProductionCountry {
+  iso_3166_1: string
+  name: string
+}
+
+interface SpokenLanguage {
+  english_name: string
+  iso_639_1: string
+  name: string
+}
+
+export default interface IMDbMovieDetails {
+  adult: boolean
+  backdrop_path: string
+  belongs_to_collection: string
+  budget: number
+  genres: IMDbGenre[]
+  homepage: string
+  id: number
+  images: {
+    backdrops: IMDbImage[]
+    logos: IMDbImage[]
+    posters: IMDbImage[]
+  }
+  imdb_id: string
+  original_language: string
+  original_title: string
+  overview: string
+  popularity: number
+  poster_path: string
+  production_companies: ProductionCompany[]
+  production_countries: ProductionCountry[]
+  release_date: string
+  revenue: number
+  runtime: number
+  spoken_languages: SpokenLanguage[]
+  status: string
+  tagline: string
+  title: string
+  video: boolean
+  vote_average: number
+  vote_count: number
+}
---
+++ src/interfaces/imdb-api/imdb-movie-image.ts
@@ -0,0 +1,6 @@
+export default interface IMDbMovieImages {
+  backdrops: IMDbImage[]
+  id: number
+  logos: IMDbImage[]
+  posters: IMDbImage[]
+}
