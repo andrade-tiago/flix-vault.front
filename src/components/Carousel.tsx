99c34e62dff472dcfd9c011b16d1fa857a729955
Tiago Andrade <tiagoandradedejoao@gmail.com>
2025-01-29 15:29:33 -0300

    :lipstick: feat(Carousel): add shadow to the sides

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -32,12 +32,16 @@ export default function Carousel({ children }: CarouselProps) {
       ref={carousel}
       className="cursor-grab overflow-hidden active:cursor-grabbing w-auto relative"
     >
+      <span className="block h-full w-20 absolute top-0 left-0 z-10 bg-gradient-to-r from-gray-950" />
+
       <motion.div
         className="inline-flex w-auto gap-10 py-4 px-3 sm:px-5 md:px-7 xl:px-9"
         drag="x"
         dragConstraints={{ right: 0, left: - width }}
         children={children}
       ></motion.div>
+
+      <span className="block h-full w-20 absolute top-0 right-0 z-10 bg-gradient-to-l from-gray-950" />
     </div>
   ) 
 }
