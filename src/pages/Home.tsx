e0b58ee4628ef0d0c3455c241e1abab989ccbc0b
Tiago Andrade <tiagoandradedejoao@gmail.com>
2025-01-29 15:33:56 -0300

    :recycle: refactor(Home): load movies and sections

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,37 +1,91 @@
 import Hero from "@/components/Hero";
-import MediaListSection from "@/components/MediaListSection";
 import FeaturedMovie from "@/components/FeaturedMovie";
 import useMovieOverviewList from "@/hooks/use-movie-overview-list";
 import { useEffect } from "react";
 import { IMDbMovieListEndpoint } from "@/enums/imdb/imdb-movie-list-endpoint";
+import Carousel from "@/components/Carousel";
+import MovieCardLoading from "@/components/Loading/MovieCardLoading";
+import MovieCard from "@/components/MovieCard";
 
 export default function HomePage() {
-  const movies = {
-    inTheaters: useMovieOverviewList(IMDbMovieListEndpoint.InTheaters),
-    popular: useMovieOverviewList(IMDbMovieListEndpoint.Popular),
-  }
+  const movieLists = [
+    useMovieOverviewList(IMDbMovieListEndpoint.Popular, 1),
+    useMovieOverviewList(IMDbMovieListEndpoint.Popular, 2),
+    useMovieOverviewList(IMDbMovieListEndpoint.Popular, 3),
+  ]
+
+  const movieListSectionClasses = "px-3 sm:px-5 md:px-7 xl:px-9 flex flex-col gap-2"
+  const tenPositionsEmptyArray = Array.from({ length: 10 })
 
   useEffect(() => {
-    document.title = 'FlixVault: seus filmes e séries favoritos'
+    document.title = 'FlixVault: seus filmes favoritos!'
   }, [])
 
   return (
-    <div className="flex flex-col gap-6">
-      {movies.inTheaters.data && movies.inTheaters.data.length > 0 && (
-        <>
-          <Hero movie={movies.inTheaters.data[0]} />
+    <div className="flex flex-col gap-12">
+      {movieLists[0].isLoading || movieLists[0].error ? (
+        <div className="h-[600px] w-full bg-gray-900 animate-pulse" />
+      ) : movieLists[0].data && (movieLists[0].data.length > 0) && (
+        <Hero movie={movieLists[0].data[0]} />
+      )}
 
-          {movies.inTheaters.data.length > 1 && (
-            <MediaListSection title="Nos cinemas" mediaList={movies.inTheaters.data.slice(2)} />
+      <section className={movieListSectionClasses}>
+        <h3 className="h3">
+          Nos cinemas
+        </h3>
+
+        <Carousel>
+          {movieLists[0].isLoading || movieLists[0].error ? (
+            tenPositionsEmptyArray.map((_, i) => (
+              <MovieCardLoading key={i} />
+            ))
+          ) : movieLists[0].data && (movieLists[0].data.length > 2) && (
+            movieLists[0].data.map((movie, i) => (
+              <MovieCard movie={movie} key={i} />
+            ))
           )}
+        </Carousel>
+      </section>
 
-          <FeaturedMovie movie={movies.inTheaters.data[1]} />
-        </>
+      {movieLists[0].data && (movieLists[0].data.length > 1) && (
+        <FeaturedMovie movie={movieLists[0].data[1]} />
       )}
 
-      {movies.popular.data && movies.popular.data.length > 0 && (
-        <MediaListSection title="Filmes em alta" mediaList={movies.popular.data} />
-      )}
+      <section className={movieListSectionClasses}>
+        <h3 className="h3">
+          Em alta
+        </h3>
+
+        <Carousel>
+          {movieLists[1].isLoading || movieLists[1].error ? (
+            tenPositionsEmptyArray.map((_, i) => (
+              <MovieCardLoading key={i} />
+            ))
+          ) : movieLists[1].data && (movieLists[1].data.length > 0) && (
+            movieLists[1].data.map((movie, i) => (
+              <MovieCard movie={movie} key={i} />
+            ))
+          )}
+        </Carousel>
+      </section>
+
+      <section className={movieListSectionClasses}>
+        <h3 className="h3">
+          Escolha do editor
+        </h3>
+
+        <Carousel>
+          {movieLists[2].isLoading || movieLists[2].error ? (
+            tenPositionsEmptyArray.map((_, i) => (
+              <MovieCardLoading key={i} />
+            ))
+          ) : movieLists[2].data && (movieLists[2].data.length > 0) && (
+            movieLists[2].data.map((movie, i) => (
+              <MovieCard movie={movie} key={i} />
+            ))
+          )}
+        </Carousel>
+      </section>
     </div>
   )
 }
