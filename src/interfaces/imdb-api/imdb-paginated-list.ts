7e2789f44d7633fadf93ab47881491539afd7b07
Tiago Andrade <tiagoandradedejoao@gmail.com>
2025-02-01 16:57:34 -0300

    :broom: cleanup: remove unnecessary instructs from imdb paginated list type

--- src/interfaces/imdb-api/imdb-paginated-list.ts
+++ src/interfaces/imdb-api/imdb-paginated-list.ts
@@ -1,4 +1,4 @@
-interface IMDbMedia {
+export interface IMDbMovie {
   adult: boolean
   backdrop_path: string
   genre_ids: number[]
@@ -9,23 +9,13 @@ interface IMDbMedia {
   poster_path: string
   vote_average: number
   vote_count: number
-}
-
-export interface IMDbMovie extends IMDbMedia {
   original_title: string
   release_date: string
   title: string
   video: boolean
 }
 
-export interface IMDbTVSeries extends IMDbMedia {
-  first_air_date: string
-  name: string
-  origin_country: string[]
-  original_name: string
-}
-
-export default interface IMDbPaginatedList<T extends IMDbMedia> {
+export default interface IMDbPaginatedList<T> {
   page: number
   results: T[]
   total_pages: number
