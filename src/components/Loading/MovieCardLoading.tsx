2553d2bd2a17ba48924fc1eb4c98463843a1ce3f
Tiago Andrade <tiagoandradedejoao@gmail.com>
2025-02-05 16:14:12 -0300

    :dizzy: refactor(MovieCard[Loading]): smoother transitions

--- src/components/Loading/MovieCardLoading.tsx
+++ src/components/Loading/MovieCardLoading.tsx
@@ -1,16 +1,25 @@
+import { AnimatePresence, motion } from 'framer-motion'
+
 function MovieCardLoading() {
   return (
-    <div className="w-44 flex flex-col gap-2">
-      <div className="w-full h-64 rounded-xl bg-gray-600/30 animate-pulse" />
+    <AnimatePresence>
+      <motion.div
+        className="w-44 flex flex-col gap-2"
+        initial={{ y: 20, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        exit={{ y: -20, opacity: 0 }}
+      >
+        <div className="w-full h-64 rounded-xl bg-gray-600/30 animate-pulse" />
 
-      <div className="w-2/3 h-4 bg-gray-600/30 rounded-md animate-pulse" />
+        <div className="w-2/3 h-4 bg-gray-600/30 rounded-md animate-pulse" />
 
-      <div className="flex justify-between">
-        <div className="h-4 w-10 bg-gray-600/30 rounded-md animate-pulse" />
-        <div className="h-4 w-10 bg-gray-600/30 rounded-md animate-pulse" />
-        <div className="h-4 w-16 bg-gray-600/30 rounded-md animate-pulse" />
-      </div>
-    </div>
+        <div className="flex justify-between">
+          <div className="h-4 w-10 bg-gray-600/30 rounded-md animate-pulse" />
+          <div className="h-4 w-10 bg-gray-600/30 rounded-md animate-pulse" />
+          <div className="h-4 w-16 bg-gray-600/30 rounded-md animate-pulse" />
+        </div>
+      </motion.div>
+    </AnimatePresence>
   )
 }
 export default MovieCardLoading
--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -2,6 +2,7 @@ import { twMerge } from "tailwind-merge"
 import PlayButton from "./PlayButton"
 import MovieOverview from "@/classes/movie-overview"
 import MovieData from "./MovieData"
+import { AnimatePresence, motion } from "framer-motion"
 
 interface CardProps {
   movie: MovieOverview
@@ -9,41 +10,48 @@ interface CardProps {
 
 export default function MovieCard({ movie }: CardProps) {
   return (
-    <div
-      className="group w-44 hover:-translate-y-1 focus-within:-translate-y-1
-      transition-transform duration-200 flex flex-col gap-1 text-gray-400"
-    >
-      <div className="h-64 rounded-xl overflow-hidden relative">
-        {movie.posterURL && (
-          <img
-            src={movie.posterURL} alt=""
-            className="absolute w-full h-full object-cover -z-10 top-0 left-0"
-          />
-        )}
+    <AnimatePresence>
+      <motion.div
+        className="
+          group w-44 hover:-translate-y-1 focus-within:-translate-y-1
+          transition-transform duration-200 flex flex-col gap-1 text-gray-400
+        "
+        initial={{ y: 20, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        exit={{ y: -20, opacity: 0 }}
+      >
+        <div className="h-64 rounded-xl overflow-hidden relative">
+          {movie.posterURL && (
+            <img
+              src={movie.posterURL} alt=""
+              className="absolute w-full h-full object-cover -z-10 top-0 left-0"
+            />
+          )}
 
-        <div
-          className="w-full h-full p-3 bg-gray-950/70 backdrop-blur-xl
-          opacity-0 group-hover:opacity-100 transition-opacity focus-within:opacity-100
-          flex flex-col justify-between items-center gap-2"
-        >
-          <p
-            className={twMerge(
-              'line-clamp-[7] w-full',
-              movie.overview ? 'text-gray-100' : 'text-gray-400',
-            )}
+          <div
+            className="w-full h-full p-3 bg-gray-950/70 backdrop-blur-xl
+            opacity-0 group-hover:opacity-100 transition-opacity focus-within:opacity-100
+            flex flex-col justify-between items-center gap-2"
           >
-            {movie.overview || 'Sinopse indisponível'}
-          </p>
+            <p
+              className={twMerge(
+                'line-clamp-[7] w-full',
+                movie.overview ? 'text-gray-100' : 'text-gray-400',
+              )}
+            >
+              {movie.overview || 'Sinopse indisponível'}
+            </p>
 
-          <PlayButton movieId={movie.id} />
+            <PlayButton movieId={movie.id} />
+          </div>
         </div>
-      </div>
 
-      <h3 className="font-medium w-full truncate" title={movie.title}>
-        {movie.title}
-      </h3>
+        <h3 className="font-medium w-full truncate" title={movie.title}>
+          {movie.title}
+        </h3>
 
-      <MovieData movie={movie} className="w-full text-sm" />
-    </div>
+        <MovieData movie={movie} className="w-full text-sm" />
+      </motion.div>
+    </AnimatePresence>
   )
 }
